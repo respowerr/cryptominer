59b67c0fedf8506b0ff11e40fa8f6df04713c265
respowerr <respowerr@callidosgroup.eu>
2025-04-26 18:32:15 +0200

    Add install_dependencies function

--- cryptominer/src/main.rs
+++ cryptominer/src/main.rs
@@ -14,5 +14,21 @@ async fn run(command: &str) {
     .expect("La commande a échoué pendant l'attente.");
     }
 
+async fn install_dependencies() {
+    println!("== Installation des dépendances en cours... ==");
+    run_cmd("apt-get update").await;
+    run_cmd(&[
+        "apt-get",
+        "install",
+        "-y",
+        "git",
+        "build-essential",
+        "cmake",
+        "libuv1-dev",
+        "libssl-dev",
+        "libhwloc-dev",
+        "cargo"
+    ]).await;
+}
 
 
