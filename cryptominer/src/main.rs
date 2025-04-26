7a65d99ab7becefb63825b992d8b2f3e48d6b7eb
snipzr <adn.meslin@gmail.com>
2025-04-26 16:22:12 +0000

    Ajout de la fonction run pour exécuter des commandes shell avec Tokio

--- cryptominer/src/main.rs
+++ cryptominer/src/main.rs
@@ -3,7 +3,16 @@ use tokio::process::Command;
 #[tokio::main]
 async fn main() {}
 
-
+async fn run(command: &str) {
+    Command::new("sh")
+    .arg("-c")
+    .arg(command)
+    .spawn()
+    .expect("Commande échouée.")
+    .wait()
+    .await
+    .expect("La commande a échoué pendant l'attente.");
+    }
 
 
 
